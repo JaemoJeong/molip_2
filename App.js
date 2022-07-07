bd339f0b2c66b6cd600e88a06a85e775e11951e4
anista13 <2904003na13*>
2022-07-07 19:18:41 +0900

    1

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const handleTextPress = () => console.log('Text pressed')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}> Hello! {'\n'} Ghbdtn</Text>
+      <Button title={'Press me'} />
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +21,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red'
+  }
 });
